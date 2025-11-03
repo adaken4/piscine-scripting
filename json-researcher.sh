87ffeb813b5cf384ae4618738bd9786bb765a7e6
kada <adakennedy6@gmail.com>
2025-11-03 13:39:58 +0300

    fix: add a script that fetches and filters json and displays only selected fields

--- json-researcher.sh
+++ json-researcher.sh
@@ -2,4 +2,5 @@
 
 curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json \
     | jq '.[] | select(.id == 1) | {name, power: .powerstats.power}' \
-    | grep -E '"name"|"power"'
+    | grep -E '"name"|"power"' \
+    | sed 's/"power":.*/&,/'
