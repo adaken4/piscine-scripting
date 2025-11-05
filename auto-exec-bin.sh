4d5bb5747c1fa7c1a9a076a8db0d5432ba1aa3df
kada <adakennedy6@gmail.com>
2025-11-05 15:50:57 +0300

    fix: add auto-exec-bin script

--- auto-exec-bin.sh
+++ auto-exec-bin.sh
@@ -3,6 +3,6 @@
 mkdir -p "$HOME/myBins"
 
 printf 'printf "Hello 01 Scripting Pool\n"' > "$HOME/myBins/01exec"
-chomd +x "$HOME/myBins/01exec"
+chmod +x "$HOME/myBins/01exec"
 
 export PATH="$HOME/myBins:$PATH"
