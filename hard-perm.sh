8f5c12cf0ca9921e5e2d87a430d146fc8b1f74d0
kada <adakennedy6@gmail.com>
2025-11-05 12:55:52 +0300

    feat: add file-perm script

---
+++ hard-perm.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+# Directory 0: dr-------x (401)
+chmod 401 hard-perm/0
+
+# File 1: -r------w- (402)
+chmod 402 hard-perm/1
+
+# File 2: -rw----r-- (604)
+chmod 604 hard-perm/2
+
+# Directory 3: drwxrwxrwx (777)
+chmod 777 hard-perm/3
+
+# File 4: -r-x--x--- (511)
+chmod 510 hard-perm/4
+
+# File 5: -r--rw---- (460)
+chmod 460 hard-perm/5
+
+# File 6: -r--rw---- (460)
+chmod 460 hard-perm/6
+
+# File 7: -r-x--x--- (511)
+chmod 510 hard-perm/7
+
+# File 8: -rw----r-- (604)
+chmod 604 hard-perm/8
+
+# File 9: -r------w- (402)
+chmod 402 hard-perm/9
+
+# Directory A: dr-------x (401)
+chmod 401 hard-perm/A
