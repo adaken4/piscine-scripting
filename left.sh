e1c90646acb4de7048a68fb87cb6115e3a99298b
kada <adakennedy6@gmail.com>
2025-11-06 12:05:05 +0300

    feat: add left exercise script

---
+++ left.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Instructions
+
+# Create a file left.sh that will pass the content of a file facts to a
+# command that will return the number of lines in that file.
+
+# Usage:
+# $ ./left.sh
+# 21
+
+wc -l < facts
