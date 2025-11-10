c343af86c17b746558c44438d68f01b8d27e6d63
kada <adakennedy6@gmail.com>
2025-11-10 12:21:50 +0300

    feat: add hello_python exercise script

---
+++ hello_python.py
@@ -0,0 +1,21 @@
+# Instructions
+# Create a file hello_python.py and write inside a function say_hello_python that returns the string "Hello Python!".
+
+# def say_hello_python():
+#     # this is a function,
+#     # write your code here
+# Usage
+# Here is a possible code to test your function. Put it in another file (ex: test.py):
+
+# from hello_python import say_hello_python
+
+# print(say_hello_python())
+# Run your test file with the following command:
+
+# $ python3 test.py
+# Hello Python!
+# $
+# test.py file should be in the same directory of hello_python.py in order to work.
+
+def say_hello_python():
+    return "Hello Python!"
