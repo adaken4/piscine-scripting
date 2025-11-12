d5dc2a2e836dfc655429560ae760b7fb438a86ba
kada <adakennedy6@gmail.com>
2025-11-12 11:10:15 +0300

    feat: add object_to_json exercise script

---
+++ object_to_json.py
@@ -0,0 +1,58 @@
+# Instructions
+
+# You just landed a new job, congrats! Your new task is to build two functions to allow new users to register to your new shiny website.
+
+# The new registration information comes as string formatted as JSON. You need to create a file object_to_json.py that will have the following functions inside:
+
+# create_new_user that will receive a dict and will return a new object of the class User provided below. To be valid, the input dict must have a username key and an email key. The new User will have the same username and email of the input dict. If the input dict is invalid, the default user will be returned.
+
+# class User:
+#     username = 'user'
+#     email = 'something@mail.com'
+# user_to_json that will receive a User and will return the object as a string in JSON format. Be aware of the Python types that can be converted to JSON!
+
+# Usage
+# Here is a possible test.py to test your functions:
+
+# from object_to_json import create_new_user, user_to_json
+
+# registration_0 = '{"username": "mario", "email": "mario@me.it"}'
+# registration_1 = '{"city": "Rome", "country": "Italy"}'
+
+# user_0 = create_new_user(registration_0)
+# user_1 = create_new_user(registration_1)
+
+# print(user_to_json(user_0))
+# print(user_to_json(user_1))
+# $ python3 test.py
+# {"username": "mario", "email": "mario@me.it"}
+# {}
+# $
+
+import json
+
+class User:
+    username = 'user'
+    email = 'something@mail.com'
+    
+def create_new_user(registration_str):
+    try:
+        data = json.loads(registration_str)
+        if 'username' in data and 'email' in data:
+            user = User()
+            user.username = data['username']
+            user.email = data['email']
+            return user
+    except json.JSONDecodeError:
+        return User()
+    return User()
+
+def user_to_json(user):
+    user_dict = {
+        'username': user.username,
+        'email': user.email
+    }
+    # If the user has default values, return an empty JSON object
+    if user.username == 'user' and user.email == 'something@mail.com':
+        return '{}'
+    return json.dumps(user_dict)
