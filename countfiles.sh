8b3c09874b2f4051745438988e9643eaba597bfd
adaken4 <adakennedy@outlook.com>
2025-11-17 22:04:53 +0300

    feat: add cl-camp6 exercise script

---
+++ countfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instructions
+
+# "Now, do your inventory"
+
+# Create a file countfiles.sh, which will print the number (and only the number) of regular files and folders contained in the current directory and its sub-folders (the current directory must be included in the count):
+
+# Usage
+# $ ./countfiles.sh | cat -e
+# 12$
+# $
+
+find . | wc -l
