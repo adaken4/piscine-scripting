ca9d1978e35731b9c30d3118f33f86ee23d0580a
adaken4 <adakennedy@outlook.com>
2025-11-17 21:26:44 +0300

    feat: add easy-perm exercise script

---
+++ easy-perm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Instructions
+
+# Create a file easy-perm.sh, which will change the default permissions for the example.txt and example2.txt files inside the folder easy-perms, to the ones below:
+
+# Expected Output:
+
+# $ ls -l easy-perm
+# total 8
+# -rwxr--rw- 1 user user 689 dez 13 16:14 example2.txt
+# -rw-r--r-- 1 user user 348 dez 13 16:14 example.txt
+# $
+
+chmod 644 easy-perms/example.txt
+chmod 746 easy-perms/example2.txt
